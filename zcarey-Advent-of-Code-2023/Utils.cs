74e0666435960e384dd4d469343b4c7d4c6b275c
Zachary Carey <zach.d.carey@gmail.com>
2023-12-04 17:37:28 -0500

    Day02 part 1

---
+++ zcarey-Advent-of-Code-2023/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zcarey_Advent_of_Code_2023
+{
+    public static class Utils
+    {
+
+        public static IEnumerable<string> GetLines(this StringReader reader)
+        {
+            string? line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                yield return line;
+            }
+        }
+
+        public static IEnumerable<string> GetLines(this string str)
+        {
+            StringReader reader = new StringReader(str);
+            return reader.GetLines();
+        }
+
+    }
+}
